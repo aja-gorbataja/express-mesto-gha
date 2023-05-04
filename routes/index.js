c0c140f8acc341cc4d41f48cfb02f8b0215cef23
aja_gorbataja <aja.gorbataja@yandex.ru>
2023-05-04 14:26:30 +0300

    13/8

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const userRouter = require('./users');
 const cardRouter = require('./cards');
 
 router.use('/users', userRouter);
-router.use('./cards', cardRouter);
+router.use('/cards', cardRouter);
 router.use('/*', (req, res) => { res.status(404).send({ message: 'Страница не найдена' }); });
 
 module.exports = router;
