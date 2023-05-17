00d7d0f1c8b52175cf0e3b49719d61065d122473
aja_gorbataja <aja.gorbataja@yandex.ru>
2023-05-18 02:50:08 +0300

    14/5

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
-const { errors } = require('celebrate');
 const router = require('./routes/index');
 
 const { PORT = 3000 } = process.env;
@@ -13,8 +12,6 @@ app.use(express.json());
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(router);
-
-app.use(errors());
 app.use((err, req, res, next) => {
   const { status = 500, message } = err;
   res.status(status).send({ message: status === 500 ? 'На сервере произошла ошибка' : message });
--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,14 @@
 const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
+const { celebrate, Joi, errors } = require('celebrate');
 const userRouter = require('./users');
 const cardRouter = require('./cards');
-const NotFoundError = require('../errors/NotFoundError');
 const auth = require('../middlewares/auth');
 const { createUser, login } = require('../controllers/users');
 
 router.use('/users', auth, userRouter);
 router.use('/cards', auth, cardRouter);
-router.use('/*', auth, (req, res, next) => {
-  next(new NotFoundError('Страница не найдена'));
-});
 
-router.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().regex(/^[0-9a-fA-F]{24}$/),
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), createUser);
+router.post('/signup', createUser);
 
 router.post('/signin', celebrate({
   body: Joi.object().keys({
@@ -28,5 +16,7 @@ router.post('/signin', celebrate({
     password: Joi.string().required(),
   }),
 }), login);
+
+router.use(errors());
 
 module.exports = router;
