3da5ef0c09b4842333fa0beae969136856b4c7d5
aja_gorbataja <aja.gorbataja@yandex.ru>
2023-05-22 12:46:36 +0300

    afterpartyFix1

--- app.js
+++ app.js
@@ -12,13 +12,13 @@ app.use(express.json());
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
+app.use(errors());
+
 app.use(router);
 app.use((err, req, res, next) => {
   const { status = 500, message } = err;
   res.status(status).send({ message: status === 500 ? 'На сервере произошла ошибка' : message });
   next();
 });
-
-app.use(errors());
 
 app.listen(PORT);
